fd5985cb3c5143dcd2da1720b2d6c126f568aa4c
Admin_SRV <Admin_SRV@Admin_SRV-PC>
2022-05-12 11:38:44 +0530

    #switch case

---
+++ MyJavaProgram/src/test/java/javaPrograms/Switchcase.java
@@ -0,0 +1,29 @@
+package javaPrograms;
+
+public class Switchcase {
+
+	public static void main(String[] args) {
+
+		int i=6;
+		
+		switch(i) {
+		case 1:
+			System.out.println("one");
+		case 2:
+			System.out.println("two");
+		case 3:	
+			System.out.println("three");
+		case 4:	
+			System.out.println("four");
+		case 5:	
+			System.out.println("five");
+		case 6:	
+			System.out.println("six");
+			
+		case 7:
+			System.out.println("seven");
+		}
+		
+	}
+
+}
