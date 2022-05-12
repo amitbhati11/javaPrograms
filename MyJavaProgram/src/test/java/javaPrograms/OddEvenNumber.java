96b6ba339e6f118bb2032a2d6f0884974158c752
Admin_SRV <Admin_SRV@Admin_SRV-PC>
2022-05-12 11:23:04 +0530

    #oddevenprogram

---
+++ MyJavaProgram/src/test/java/javaPrograms/OddEvenNumber.java
@@ -0,0 +1,20 @@
+package javaPrograms;
+
+public class OddEvenNumber {
+	
+	public static void main(String [] args) {
+		
+		int i=5;
+		
+		
+		if(i==0) {
+			System.out.println("nothing");
+		}else if(i%2==0) {
+			System.out.println("even moumber");
+		}else {
+			System.out.println("odd");
+		}
+			
+	}
+
+}
