fbbf3f14e28a56bb941046dcc87a4a83159b2575
Admin_SRV <Admin_SRV@Admin_SRV-PC>
2022-05-15 17:03:38 +0530

    #AcessModifier

---
+++ MyJavaProgram/src/test/java/javaPrograms/AcessModifier.java
@@ -0,0 +1,21 @@
+package javaPrograms;
+
+//we can create private,protected  in innerclass not outercalss
+//Public –public class can access outside the pkg.(anyclass or pakg)
+//Default class-can be access inside the pkg.(Specific pkg)(pkg private)
+//Private- can be access in specific class
+//Protected-subsiding class( can be access other pkg subclasses)
+
+
+ class school{
+	
+}
+
+public class AcessModifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
