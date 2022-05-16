76e5eb631b79867b9d4a6d801defc9152c36dfb2
Admin_SRV <Admin_SRV@Admin_SRV-PC>
2022-05-16 08:25:42 +0530

    #scanner#trycatch

---
+++ MyJavaProgram/src/test/java/javaPrograms/Scannerclass.java
@@ -0,0 +1,22 @@
+package javaPrograms;
+
+import java.util.Scanner;
+
+public class Scannerclass {
+
+	public static void main(String [] args) {
+		
+		int content;
+		
+		System.out.println("Enter the number:");
+		
+		Scanner sc=new Scanner(System.in);
+		
+		//content=sc.nextInt();
+		
+		//System.out.println(content);
+		System.out.println("enter the line");
+		 String st= sc.nextLine();
+		 System.out.println(st);
+	}
+}
